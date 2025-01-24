8606c443e666dc51ea406c0219f93769698918c9
braj <braj.shan026@gmail.com>
2025-01-24 16:34:21 +0530

    Added Module Audit

--- src/main/java/com/gl/Application.java
+++ src/main/java/com/gl/Application.java
@@ -5,14 +5,33 @@
  */
 package com.gl;
 
+import com.gl.Audit.ModulesAudit;
 import com.gl.Config.MySQLConnection;
+import com.gl.Config.PropertyReader;
 import com.gl.P5Process.CustomImeiPairProcess;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class Application {
+    public static PropertyReader propertyReader = new PropertyReader();
+    static Logger log = LogManager.getLogger(Application.class);
+    public static String audDb;
+    public static String serverName;
+
     public static void main(String[] args) {
+        int auditId = 0;
+        long startTime = System.currentTimeMillis();
+        try {
+            audDb = propertyReader.getConfigPropValue("auddbName");
+            serverName = propertyReader.getConfigPropValue("serverName");
+        } catch (Exception e) {
+            log.error("Not able to fetch information for serverName " + e);
+        }
         try (var c = new MySQLConnection().getConnection()) {
+            auditId = ModulesAudit.insertModuleAudit(c, "customs_duty_module", "Process for customs_duty_module", serverName);
             CustomImeiPairProcess.p5(c);
+
         } catch (Exception e) {  
         }
        // CustomImeiPairProcess.p5(new MySQLConnection().getConnection());
